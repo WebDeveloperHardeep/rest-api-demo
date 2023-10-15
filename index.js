39fd565d679bbb4a78af65008c5cddea05830aee
Hardeep <mailto:deshardeep@gmail.com>
2023-10-15 23:48:55 +0530

    add register route and implement registtration

--- index.js
+++ index.js
@@ -13,15 +13,59 @@ app.use((req, res, next) => {
   // Move to the next middleware or route handler
 });
 
+// localhost:3000"/" , homepage route
 app.get("/", function (req, res) {
   res.send("hellow");
 });
 
+// localhost:3000"/login", login route
 app.post("/login", (req, res) => {
-  const username = req.username;
-  const password = req.password;
+  console.log({ req: req.body });
 
-  res.send("helo");
+  const uname = req.body.username;
+  const upass = req.body.password;
+
+  const sqlQuery = `SELECT * FROM users WHERE username='${uname}' AND password='${upass}'`;
+
+  console.log({ sqlQuery });
+
+  db.query(sqlQuery, async (err, results) => {
+    if (err) {
+      console.log("error running query");
+      res.send("error login");
+    }
+
+    if (results && results.length > 0) {
+      console.log(results);
+      res.send("login info is correct");
+    } else {
+      res.send("login info is not correct");
+    }
+  });
+
+  console.log("hello");
+});
+
+app.post("/register", (req, res) => {
+  // get details fom requests
+  const user_name = req.body.username;
+  const user_pass = req.body.password;
+
+  // create sqlQuery (INSERT QUERY)
+  const sqlInsertQuery = `INSERT INTO users (username, password) VALUE ('${user_name}', '${user_pass}');`;
+
+  // execute query
+  db.query(sqlInsertQuery, async (err, results) => {
+    // handle error or handle results
+    if (err) {
+      console.log("error running InSERT query");
+      console.log({ err });
+      res.send("error register");
+    } else {
+      res.send("Register success");
+    }
+    // we will not check if(results), because insert query does not returns rows, only success or error
+  });
 });
 
 app.listen(3000, () => {
